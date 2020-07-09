8b993884000122698d97c31b40224f0c51078339
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-07-09 10:57:07 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var mce2 = localStorage.getItem("mc2");
 var mce3 = localStorage.getItem("mc3");
 var mce4 = localStorage.getItem("mc4");
 var mce5 = localStorage.getItem("mc5");
+var mce6 = localStorage.getItem("mc6");
 a.innerHTML = localStorage.getItem("score");
 a.innerHTML = parseInt(a.innerHTML);
 if (a.innerHTML == undefined){
@@ -37,7 +38,14 @@ if (mce5 == "d"){
 	var mouse5 = document.querySelector(".mouse5");
 	mouse5.setAttribute("disabled", "");
 }
-
+if (mce6 == "d"){
+	var mouse6 = document.querySelector(".mouse6");
+	mouse6.setAttribute("disabled", "");
+	setInterval(function(){
+			a.innerHTML++;
+			localStorage.setItem("score", a.innerHTML);
+		}, 5000);
+}
 
 
 function getScore(){
@@ -107,6 +115,19 @@ function godMouse(){
 	}
 }
 
+function autoClicker(){
+	if (a.innerHTML >= 1000){
+		a.innerHTML-=1000;
+		var mouse6 = document.querySelector(".mouse6");
+		mouse6.setAttribute("disabled", "");
+		localStorage.setItem("mc6", "d");
+		autoClick = setInterval(function(){
+			a.innerHTML++;
+			localStorage.setItem("score", a.innerHTML);
+		}, 5000);
+	}
+}
+
 function clearSave(){
 	a.innerHTML = 0
 	scorepoint = 1;
@@ -117,6 +138,7 @@ function clearSave(){
 	localStorage.setItem("mc3", "")
 	localStorage.setItem("mc4", "")
 	localStorage.setItem("mc5", "")
+	localStorage.setItem("mc6", "");
 
 	var mouse1 = document.querySelector(".mouse1");
 	mouse1.outerHTML = mouse1.outerHTML.replace("disabled", "");
@@ -128,4 +150,8 @@ function clearSave(){
 	mouse1.outerHTML = mouse1.outerHTML.replace("disabled", "");
 	mouse1 = document.querySelector(".mouse5");
 	mouse1.outerHTML = mouse1.outerHTML.replace("disabled", "");
+	mouse1 = document.querySelector(".mouse6");
+	mouse1.outerHTML = mouse1.outerHTML.replace("disabled", "");
+
+	clearInterval(autoClick);
 }
